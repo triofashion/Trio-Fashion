dbb0caae983ae32da25525fa552a93a8f1436643
triofashion <triofashion93@gmail.com>
2026-06-13 03:28:10 +0600

    Add files via upload

---
+++ Trio Fashion/products.js
@@ -0,0 +1,73 @@
+  // Product Data
+        const products = [
+            {
+                id: 1,
+                title: "Pakistani Kof Hata Premium Quality Panjabi",
+                originalPrice: 2090,
+                discountedPrice: 1690,
+                discountPercent: 24,
+                category: "Panjabi",
+                image:"./pakistani-kof-hata.png",
+                iconColor: "text-blue-600"
+            },
+            {
+                id: 2,
+                title: "Premium Pakistani Stiz Fabric Kof Hata Panjabi",
+                originalPrice: 2000,
+                discountedPrice: 1590,
+                discountPercent: 26,
+                category: "panjabi",
+                image:"./stiz-kof-hata.png",
+                color: "bg-indigo-100",
+                iconColor: "text-indigo-600"
+            },
+            {
+                id: 3,
+                title: "Premium Quality Fabric Right Shoulder Design Panjabi",
+                originalPrice: 2100,
+                discountedPrice: 1750,
+                discountPercent: 20,
+                category: "panjabi",
+                image:"./buke-design-panjabi.png",
+                color: "bg-stone-100",
+                iconColor: "text-stone-600"
+            },
+            {
+                id: 4,
+                title: "Premium Stiz Fabric Panjabi",
+                originalPrice: 1788,
+                discountedPrice: 1490,
+                discountPercent: 20,
+                category: "panjabi",
+                image:"./stiz-panjabi.png",
+                color: "bg-rose-100",
+                iconColor: "text-rose-500"
+            },
+            {
+                id: 5,
+                title: "Premium Quality Fabric Pure Black Panjabi",
+                originalPrice: 2100,
+                discountedPrice: 1750,
+                discountPercent: 20,
+                category: "panjabi",
+                image:"./black-panjabi.png",
+                color: "bg-amber-100",
+                iconColor: "text-amber-600"
+            },
+            {
+                id: 6,
+                title: "Premium Quality Fabric White Panjabi",
+                originalPrice: 1390,
+                discountedPrice: 1190,
+                discountPercent: 17,
+                category: "panjabi",
+                image:"./white-panjabi.jpeg",
+                color: "bg-emerald-100",
+                iconColor: "text-emerald-600"
+            }
+        ];
+
+
+
+   
+
